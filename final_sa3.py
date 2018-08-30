011cefcaba97656adeddf200458df4f19ea2d8a3
jiangchao08 <jiangchao08@baidu.com>
2018-08-30 08:40:25 +0800

    add final

---
+++ final_sa3.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2017 Baidu.com, Inc. All Rights Reserved
+#
+
+"""
+Time    : 18/8/12 22:06
+Author  : jiangchao08
+Site    : 
+File    : sa.py
+Software: PyCharm Community Edition
+
+"""
+import common.fitness as fitness
+import common.final.produce_seed as produce_seed
+
+from common.exe_time import exeTime
+import array
+import random
+import numpy as np
+import pandas as pd
+import data_preprocess.data_process as data_process
+import static.static_info as static_tool
+import math
+
+alpha = 10
+beta = 0.5
+T = 98
+
+machinesMap, sortedMachineList = data_process.handle_machine(
+    'data/final/machine_resources.a.csv')
+for machineId, machine in sortedMachineList:
+    print(machineId)
+appsMap, sortedAppList = data_process.handle_app(
+    'data/final/app_resources.csv')
+instancesMap, sortedInstanceList = data_process.handle_instance(
+    'data/final/instance_deploy.a.csv')
+instance_interferences = data_process.handle_app_interference(
+    'data/final/app_interference.csv')
+
+jobsMap, sortedJobList = data_process.handle_job('data/final/job_info.a.csv')
+
+
+def read_init_solution(sortedInstanceList):
+    machine_instances_map = {}
+    unassigned_machines = {}
+    instance_machine_map = {}
+    for machineId, machine in sortedMachineList:
+        machine_instances_map[machineId] = []
+    unassigned_instances = []
+    assign_count = 0
+    for instanceId, instance in sortedInstanceList:
+        if instance.machineId is None:
+            unassigned_instances.append(instanceId)
+        else:
+            assign_count += 1
+            instance.final_machineId = instance.machineId
+            instance_machine_map[instanceId] = instance.machineId
+            machine_instances_map[instance.machineId].append(instance)
+
+    fit = fitness.fitnessfun(machine_instances_map, machinesMap, appsMap, assign_count,
+                             len(instancesMap))
+    for machineId, instances in machine_instances_map.items():
+        if len(instances) == 0:
+            unassigned_machines[machineId] = []
+    return machine_instances_map, instance_machine_map, fit, unassigned_machines
+
+
+machine_instances_map, instance_machine_map, fit, unassigned_machines = read_init_solution(sortedInstanceList)
+print(fit)
+origin_fit = fit
+
+
+# residual_machine_p, residual_machine_m, residual_machine_pm, residual_machine_disk, \
+# residual_machine_mem, used_machine_cpu, residual_machine_cpu, machine_apps_num_map, machine_cpu_score = compute_residual_info(
+#     machine_instances_map)
+
+
+machine_jobs, _ = produce_seed.randomGreedy(sortedJobList, jobsMap, unassigned_machines, machinesMap, appsMap, sortedMachineList)
+
+res_file = open('2018-8-29-final-a-1.0-res.csv', 'w')
+for (machineId, jobId, start_timme), nums in machine_jobs:
+    res_file.write(str(jobId) + ',' + str(machineId) + str(start_timme) + str(nums) + '\n')
+res_file.close()
